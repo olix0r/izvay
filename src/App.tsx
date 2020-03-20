aa7087c5b3b4a1f11677bbab04e3368257f78e49
Oliver Gould <ver@buoyant.io>
2020-03-20 01:56:03 +0000

    wip: sorta broke

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import { group } from "d3-array";
 import React, { FunctionComponent, useCallback, useEffect, useState } from "react";
 import styled from "@emotion/styled";
 
@@ -25,6 +26,7 @@ interface Report extends Fortio.Report {
   protocol: Protocol;
   rate: number;
   direction: Direction;
+  build?: string;
 }
 
 interface LabelReport {
@@ -38,120 +40,116 @@ async function getReports(): Promise<Reports> {
 }
 
 const SvgStyled = styled.svg`
+  height: 100%;
   width: 100%;
   color: #0f0;
-  background-color: #111;
+  background-color: #121212;
   padding: 20px;
 `;
 
 const rowHeight = 20;
-const margin = {
-  top: rowHeight,
-  bottom: 0,
-  left: 100,
-  right: 250
-};
 
-const LatencyHeatmap: FunctionComponent<{ reports: Reports, labeler: LabelReport }> = ({ reports, labeler }) => {
-  let drawReports = (element: SVGSVGElement) => {
-    console.log("Drawing reports", element, reports);
-    if (reports.length === 0) {
-      return;
-    }
+const LatencyHeatmap: FunctionComponent<{ reports: Reports, labeler: LabelReport }> =
+  ({ reports, labeler }) => {
+    let drawReports = (element: SVGSVGElement) => {
+      console.log("Drawing reports", element, reports);
+      if (reports.length === 0) {
+        return;
+      }
 
-    const { height, width } = element.getBoundingClientRect();
+      const { width } = element.getBoundingClientRect();
+      const height = (rowHeight + 2) * reports.length;
 
-    const maxLatency = d3.max(reports, r =>
-      d3.max(r.DurationHistogram.Data, d => d.End)
-    );
-    console.log(maxLatency!);
-    const x = d3
-      .scaleLinear()
-      .domain([0, maxLatency!])
-      .rangeRound([margin.left, width - margin.left - margin.right]);
+      const maxLatency = d3.max(reports, r =>
+        d3.max(r.DurationHistogram.Data, d => d.End)
+      );
+      const x = d3
+        .scaleLinear()
+        .domain([0, maxLatency!])
+        .rangeRound([200, width]);
 
-    const y = d3
-      .scaleBand()
-      .domain(reports.map(labeler))
-      .rangeRound([margin.top, margin.top + reports.length * rowHeight]);
+      const y = d3
+        .scaleBand()
+        .domain(reports.map(labeler))
+        .rangeRound([rowHeight, height - rowHeight]);
 
-    reports.forEach(r => {
-      r.DurationHistogram.Data.forEach(d => {
-        console.log(r.protocol, r.direction, r.rate, d, x(d.Start), x(d.End));
-      });
-    });
+      // reports.forEach(r => {
+      //   r.DurationHistogram.Data.forEach(d => {
+      //     console.log(r.protocol, r.direction, r.rate, d, x(d.Start), x(d.End));
+      //   });
+      // });
 
-    const svg = d3
-      .select(element)
-      //.attr('width', width)
-      //.attr('height', margin.top + margin.bottom + reports.length * rowHeight);
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", `0 0 ${width} ${height}`);
+      const svg = d3
+        .select(element)
+        //.attr('width', width)
+        //.attr('height', margin.top + margin.bottom + reports.length * rowHeight);
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", `0 0 ${width} ${height}`);
 
-    const maxCount = d3.max(reports, r =>
-      d3.max(r.DurationHistogram.Data, d => d.Count)
-    );
-    const boxColor = d3
-      .scaleSequential(d3.interpolateCool)
-      .domain([0, Math.pow(maxCount!, 0.5)]);
-    const barColor = d3
-      .scaleOrdinal(d3.schemeYlOrRd[5])
-      .domain(["50", "75", "90", "99", "99.9"]);
+      const maxCount = d3.max(reports, r =>
+        d3.max(r.DurationHistogram.Data, d => d.Count)
+      );
+      const boxColor = d3
+        .scaleSequential(d3.interpolateCool)
+        .domain([0, Math.pow(maxCount!, 0.5)]);
+      const barColor = d3
+        .scaleOrdinal(d3.schemeYlOrRd[5])
+        .domain(["50", "75", "90", "99", "99.9"]);
 
-    svg.append("g").call(g =>
-      g
-        .attr("transform", `translate(0,${margin.top})`)
-        .call(
-          d3
-            .axisTop(x)
-            .tickSize((width - margin.left - margin.right) / 100)
-            .tickFormat(n => `${n.valueOf() * 1000}ms`)
-        )
-        .call(g => g.selectAll(".domain").remove())
-    );
+      svg.append("g").call(g =>
+        g
+          .attr("transform", `translate(0,0)`)
+          .call(
+            d3
+              .axisTop(x)
+              .tickSize(width / 100)
+              .tickFormat(n => `${n.valueOf() * 1000}ms`)
+          )
+          .call(g => g.selectAll(".domain").remove())
+      );
 
-    svg.append("g").call(g =>
-      g
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y).tickSizeOuter(0))
-        .call(g => g.selectAll(".domain").remove())
-    );
+      svg.append("g").call(g =>
+        g
+          .attr("transform", `translate(0,0)`)
+          .call(d3.axisLeft(y).tickSizeOuter(0))
+          .call(g => g.selectAll(".domain").remove())
+      );
 
-    const row = svg
-      .append("g")
-      .selectAll("g")
-      .data(reports)
-      .join("g")
-      .attr("transform", r => `translate(0,${y(labeler(r))})`);
+      const row = svg
+        .append("g")
+        .selectAll("g")
+        .data(reports)
+        .join("g")
+        .attr("transform", r => `translate(0,${y(labeler(r))})`);
 
-    row
-      .append("g")
-      .selectAll("rect")
-      .data(report => report.DurationHistogram.Data)
-      .join("rect")
-      .attr("x", d => x(d.Start) + 1)
-      .attr("width", d => x(d.End) - x(d.Start) - 1)
-      .attr("height", y.bandwidth() - 1)
-      .attr("fill", d => boxColor(Math.pow(d.Count, 0.5)))
-      .append("title")
-      .text(d => `${d.Count} reqs [${d.Start}ms..${d.End}ms)`);
+      row
+        .append("g")
+        .selectAll("rect")
+        .data(report => report.DurationHistogram.Data)
+        .join("rect")
+        .attr("x", d => x(d.Start) + 1)
+        .attr("width", d => x(d.End) - x(d.Start) - 1)
+        .attr("height", y.bandwidth() - 1)
+        .attr("fill", d => boxColor(Math.pow(d.Count, 0.5)))
+        .append("title")
+        .text(d => `${d.Count} reqs [${d.Start}ms..${d.End}ms)`);
 
-    row
-      .append("g")
-      .selectAll("rect")
-      .data(r => r.DurationHistogram.Percentiles)
-      .join("rect")
-      .attr("x", p => x(p.Value))
-      .attr("width", 3)
-      .attr("height", y.bandwidth() - 1)
-      .attr("fill", p => barColor(`${p.Percentile}`))
-      .append("title")
-      .text(p => `${p.Percentile} percentile ${p.Value}ms`);
+      row
+        .append("g")
+        .selectAll("rect")
+        .data(r => r.DurationHistogram.Percentiles)
+        .join("rect")
+        .attr("x", p => x(p.Value))
+        .attr("width", 3)
+        .attr("height", y.bandwidth() - 1)
+        .attr("fill", p => barColor(`${p.Percentile}`))
+        .append("title")
+        .text(p => `${p.Percentile} percentile ${p.Value}ms`);
+    };
+
+    console.log("Returning svg");
+    return <SvgStyled ref={useCallback(drawReports, [reports])} />;
   };
-
-  console.log("Returning svg");
-  return <SvgStyled ref={useCallback(drawReports, [reports])} />;
-};
 
 const App: FunctionComponent = () => {
   const [reports, setReports] = useState<Reports>([]);
@@ -168,11 +166,22 @@ const App: FunctionComponent = () => {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth='xl'>
-        <GridList>
-            <LatencyHeatmap
-              reports={reports}
-              labeler={(report) => report.rate.toString()}
-            />
+        <GridList cols={2}>
+          {Array.from(group(reports, r => r.protocol).values()).flatMap(reports => {
+            return Array.from(group(reports, r => r.direction).values()).flatMap(reports => {
+              return Array.from(group(reports, r => r.rate).values()).map(reports => {
+                return (
+                  <GridListTile cols={1} key={`${reports[0].protocol}-${reports[0].direction}-${reports[0].rate}`}>
+                    <GridListTileBar title={`${reports[0].protocol} ${reports[0].direction} ${reports[0].rate}rps`} />
+                    <LatencyHeatmap
+                      reports={reports}
+                      labeler={({ build }) => build ? `${build}` : 'latest'}
+                    />
+                  </GridListTile>
+                );
+              });
+            });
+          })}
         </GridList>
       </Container>
     </React.Fragment>
