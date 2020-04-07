968b80c1323952331c883a59e4b9c2104e9621ef
Oliver Gould <ver@buoyant.io>
2020-04-07 23:39:59 +0000

    Updates

---
+++ precache-manifest.a4e7eff2d6c8ae470b004228e636cd1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77506cdd94a34e37263fb06a8c2b743c",
+    "url": "/izvay/index.html"
+  },
+  {
+    "revision": "28c69dc85e969c2b973b",
+    "url": "/izvay/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af6f5b4ee46742dd1a11",
+    "url": "/izvay/static/js/2.bc8f4251.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/izvay/static/js/2.bc8f4251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c69dc85e969c2b973b",
+    "url": "/izvay/static/js/main.aa8eda90.chunk.js"
+  },
+  {
+    "revision": "91681a9d09eaddeddade",
+    "url": "/izvay/static/js/runtime-main.8c0fb332.js"
+  }
+]);
